5093a241ff58043b3dfd5c418a4fdc8d557db4cf
jhpy1024 <jhpy1024@gmail.com>
2014-07-17 21:22:29 +0100

    Added support for moving/zooming view

--- include/Application.hpp
+++ include/Application.hpp
@@ -16,21 +16,34 @@ class Application
         void draw();
 
     private:
-        void handleKeyPress(const sf::Event& event);
         void loadTextures();
         void createMap();
+
+        void handleKeyPress(const sf::Event& event);
+        void moveUp();
+        void moveDown();
+        void moveLeft();
+        void moveRight();
+        void zoomIn();
+        void zoomOut();
+        void resetZoom();
 
     private:
         const int WIDTH;
         const int HEIGHT;
+        const int MAP_WIDTH;
+        const int MAP_HEIGHT;
+        const float TILE_SIZE;
+        const float ZOOM_FACTOR;
 
         sf::RenderWindow m_Window;
+        sf::View m_View;
         
         sf::Texture m_Grass;
         sf::Texture m_Water;
         sf::Texture m_Sand;
         sf::Texture m_Stone;
-        std::vector<sf::RectangleShape> m_Sprites;
+        std::vector<std::vector<sf::RectangleShape>> m_Sprites;
 
         PerlinNoise m_PerlinNoise;
 };
--- src/Application.cpp
+++ src/Application.cpp
@@ -1,9 +1,16 @@
 #include "Application.hpp"
 
+#include <iostream>
+
 Application::Application(int width, int height)
     : WIDTH(width)
     , HEIGHT(height)
+    , MAP_WIDTH(500)
+    , MAP_HEIGHT(MAP_WIDTH)
+    , TILE_SIZE(10.f)
+    , ZOOM_FACTOR(0.1f)
     , m_Window(sf::VideoMode(width, height), "Perlin Noise", sf::Style::Close)
+    , m_View({ WIDTH / 2.f, HEIGHT / 2.f }, { WIDTH, HEIGHT })
 {
     loadTextures();
     createMap();
@@ -46,9 +53,23 @@ void Application::update()
 void Application::draw()
 {
     m_Window.clear();
+    m_Window.setView(m_View);
 
-    for (auto& sprite : m_Sprites)
-        m_Window.draw(sprite);
+    for (unsigned y = 0; y < m_Sprites.size(); ++y)
+    {
+        for (unsigned x = 0; x < m_Sprites[y].size(); ++x)
+        {
+            auto viewTopLeft = sf::Vector2f(m_View.getCenter().x - m_View.getSize().x / 2.f, m_View.getCenter().y - m_View.getSize().y / 2.f);
+
+            bool outOfBounds = x * TILE_SIZE < viewTopLeft.x
+                            || y * TILE_SIZE < viewTopLeft.y
+                            || x * TILE_SIZE > viewTopLeft.x + m_View.getSize().x
+                            || y * TILE_SIZE > viewTopLeft.y + m_View.getSize().y;
+
+            if (!outOfBounds)
+                m_Window.draw(m_Sprites[y][x]);
+        }
+    }
 
     m_Window.display();
 }
@@ -60,9 +81,81 @@ void Application::handleKeyPress(const sf::Event& event)
         case sf::Keyboard::Space:
             createMap();
             break;
+        case sf::Keyboard::H:
+        case sf::Keyboard::Left:
+            moveLeft();
+            break;
+        case sf::Keyboard::L:
+        case sf::Keyboard::Right:
+            moveRight();
+            break;
+        case sf::Keyboard::J:
+        case sf::Keyboard::Down:
+            moveDown();
+            break;
+        case sf::Keyboard::K:
+        case sf::Keyboard::Up:
+            moveUp();
+            break;
+        case sf::Keyboard::I:
+            zoomIn();
+            break;
+        case sf::Keyboard::O:
+            zoomOut();
+            break;
+        case sf::Keyboard::P:
+            resetZoom();
+            break;
         default:
             break;
     }
+}
+
+void Application::moveUp()
+{
+    auto viewTopLeft = sf::Vector2f(m_View.getCenter().x - m_View.getSize().x / 2.f, m_View.getCenter().y - m_View.getSize().y / 2.f);
+
+    if (viewTopLeft.y > 0.f)
+        m_View.move(0.f, -TILE_SIZE);
+}
+
+void Application::moveDown()
+{
+    auto viewTopLeft = sf::Vector2f(m_View.getCenter().x - m_View.getSize().x / 2.f, m_View.getCenter().y - m_View.getSize().y / 2.f);
+
+    if (viewTopLeft.y + m_View.getSize().y < TILE_SIZE * MAP_HEIGHT)
+        m_View.move(0.f, TILE_SIZE);
+}
+
+void Application::moveLeft()
+{
+    auto viewTopLeft = sf::Vector2f(m_View.getCenter().x - m_View.getSize().x / 2.f, m_View.getCenter().y - m_View.getSize().y / 2.f);
+
+    if (viewTopLeft.x > 0.f)
+        m_View.move(-TILE_SIZE, 0.f);
+}
+
+void Application::moveRight()
+{
+    auto viewTopLeft = sf::Vector2f(m_View.getCenter().x - m_View.getSize().x / 2.f, m_View.getCenter().y - m_View.getSize().y / 2.f);
+
+    if (viewTopLeft.x + m_View.getSize().x < TILE_SIZE * MAP_WIDTH)
+        m_View.move(TILE_SIZE, 0.f);
+}
+
+void Application::zoomIn()
+{
+    m_View.zoom(1.f - ZOOM_FACTOR);
+}
+
+void Application::zoomOut()
+{
+    m_View.zoom(1.f + ZOOM_FACTOR);
+}
+
+void Application::resetZoom()
+{
+    m_View.setSize(WIDTH, HEIGHT);
 }
 
 void Application::loadTextures()
@@ -78,20 +171,21 @@ void Application::createMap()
     m_PerlinNoise.setSeed((float) rand() / 1000);
 
     m_Sprites.clear();
-    m_Sprites.reserve((WIDTH / 50) * (HEIGHT / 50));
 
     sf::RectangleShape sprite;
-    for (int x = 0; x < WIDTH / 10; ++x)
+    for (int y = 0; y < MAP_HEIGHT; ++y)
     {
-        for (int y = 0; y < HEIGHT / 10; ++y)
+        std::vector<sf::RectangleShape> row;
+
+        for (int x = 0; x < MAP_WIDTH; ++x)
         {
             float noise = m_PerlinNoise(x, y, 5.f, 0.5f, 2);
             float color = noise * 128.f + 128.f;
             if (color > 255.f) color = 255.f;
             else if (color < 0.f) color = 0.f;
 
-            sprite.setPosition(x * 10.f, y * 10.f);
-            sprite.setSize({ 10.f, 10.f });
+            sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+            sprite.setSize({ TILE_SIZE, TILE_SIZE });
 
             if (color < 140)
                 sprite.setTexture(&m_Water);
@@ -104,7 +198,9 @@ void Application::createMap()
             else 
                 sprite.setTexture(&m_Stone);
 
-            m_Sprites.push_back(sprite);
+            row.push_back(sprite);
         }
+
+        m_Sprites.push_back(row);
     }
 }
